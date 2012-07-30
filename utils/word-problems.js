199420c0507641ca15e4be4e8950d2a82c28ae15
HorseT <horset@gmail.com>
2012-07-30 21:07:29 +0800

    Replace files with translated version.

--- utils/word-problems.js
+++ utils/word-problems.js
@@ -1,4 +1,4 @@
-// Example usage:
+﻿// Example usage:
 // <var>person(1)</var> traveled 5 mi by <var>vehicle(1)</var>. Let
 // <var>his(1)</var> average speed be <var>personVar(1)</var>.
 // Let <var>person(2)</var>'s speed be <var>personVar(2)</var>.
@@ -9,7 +9,7 @@
 $.extend(KhanUtil, {
     toSentence: function(array, conjunction) {
         if (conjunction == null) {
-            conjunction = "and";
+            conjunction = "和";
         }
 
         if (array.length === 0) {
@@ -51,16 +51,17 @@ $.extend(KhanUtil, {
     //        - return "word"
     plural: (function() {
         var oneOffs = {
-            "quiz": "quizzes",
-            "shelf": "shelves",
-            "loaf": "loaves",
-            "potato": "potatoes",
-            "person": "people",
-            "is": "are",
-            "was": "were",
-            "foot": "feet",
-            "square foot": "square feet",
-            "tomato": "tomatoes"
+            "測驗": "測驗",
+	//        中文沒有單複數的問題，故這個部份可以免除？
+    //        "shelf架子": "shelves架子",
+    //        "loaf麵包": "loaves麵包",
+    //        "potato馬鈴薯": "potatoes馬鈴薯",
+    //        "person人": "people人",
+    //        "is是": "are是",
+    //        "was是": "were是",
+    //        "foot腳": "feet腳",
+    //        "square foot平方公尺": "square feet平方公尺",
+    //        "tomato番茄": "tomatoes番茄"
         };
 
         var pluralizeWord = function(word) {
@@ -101,17 +102,17 @@ $.extend(KhanUtil, {
             else {
                 // "-y" => "-ies"
                 if (/[^aeiou]y$/i.test(word)) {
-                    word = word.replace(/y$/i, "ies");
+                    word = word.replace(/y$/i, ""); //"ies"
                 }
 
                 // add "es"; things like "fish" => "fishes"
                 else if (/[sxz]$/i.test(word) || /[bcfhjlmnqsvwxyz]h$/.test(word)) {
-                    word += "es";
+                    word += ""; //"es"
                 }
 
                 // all the rest, just add "s"
                 else {
-                    word += "s";
+                    word += ""; //"s"
                 }
 
                 if (isUpperCase) {
@@ -127,7 +128,7 @@ $.extend(KhanUtil, {
 
                 // if no extra args, just add "s" (if plural)
                 if (arguments.length === 1) {
-                    return usePlural ? "s" : "";
+                    return usePlural ? "" : ""; //"s" : ""
                 }
 
                 if (usePlural) {
@@ -145,21 +146,7 @@ $.extend(KhanUtil, {
                 return usePlural ? plural : value;
             }
         };
-    })(),
-
-    // Pluralize with a code tag around the number
-    // - pluralTex(NUMBER, singular):
-    //        - if necessary, magically pluralize <singular>
-    //        - return "<code>NUMBER</code> word"
-    // - pluralTex(NUMBER, singular, plural):
-    //        - return "<code>NUMBER</code> word"
-    pluralTex: function(value, arg1, arg2) {
-        if (typeof arg2 === "string") {
-            return "<code>" + value + "</code> " + KhanUtil.plural(arg1, arg2, value);
-        } else {
-            return "<code>" + value + "</code> " + KhanUtil.plural(arg1, value);
-        }
-    }
+    })()
 });
 
 $.fn["word-problemsLoad"] = function() {
@@ -197,25 +184,25 @@ $.fn["word-problemsLoad"] = function() {
     };
 
     var names = [
-        ["Ashley", "f"],
-        ["Brandon", "m"],
-        ["Ben", "m"],
-        ["Christopher", "m"],
-        ["Daniel", "m"],
-        ["Emily", "f"],
-        ["Gabriela", "f"],
-        ["Ishaan", "m"],
-        ["Jessica", "f"],
-        ["Kevin", "m"],
-        ["Luis", "m"],
-        ["Michael", "m"],
-        ["Nadia", "f"],
-        ["Omar", "m"],
-        ["Stephanie", "f"],
-        ["Tiffany", "f"],
-        ["Umaima", "f"],
-        ["Vanessa", "f"],
-        ["William", "m"]
+        ["小英", "f"],
+        ["小明", "m"],
+        ["學諒", "m"],
+        ["新舟", "m"],
+        ["偉誠", "m"],
+        ["凱辰", "f"],
+        ["芹萱", "f"],
+        ["子恩", "m"],
+        ["一寧", "f"],
+        ["季玄", "m"],
+        ["宗宇", "m"],
+        ["皓瑋", "m"],
+        ["子儀", "f"],
+        ["翔中", "m"],
+        ["沂玲", "f"],
+        ["怡婷", "f"],
+        ["巧筠", "f"],
+        ["鎔毓", "f"],
+        ["柏清", "m"]
     ];
 
     // We only want one name per letter of the alphabet, so group people with
@@ -228,27 +215,26 @@ $.fn["word-problemsLoad"] = function() {
     people = new IncrementalShuffler(people);
 
     var vehicles = new IncrementalShuffler([
-        "bike",
-        "car",
-        "horse",
-        "motorcycle",
-        "scooter",
-        "train"
+        "腳踏車",
+        "汽車",
+        "馬",
+        "機車",
+        "火車"
     ]);
 
     var courses = new IncrementalShuffler([
-        "algebra",
-        "chemistry",
-        "geometry",
-        "history",
-        "physics",
-        "Spanish"
+        "代數",
+        "化學",
+        "幾何學",
+        "歷史",
+        "物理",
+        "英文"
     ]);
 
     var exams = new IncrementalShuffler([
-        "exam",
-        "test",
-        "quiz"
+        "考試",
+        "考試",
+        "測驗"
     ]);
 
     var binops = new IncrementalShuffler([
@@ -270,187 +256,184 @@ $.fn["word-problemsLoad"] = function() {
     ]);
 
     var collections = new IncrementalShuffler([
-        ["chair", "row", "make"],
-        ["party favor", "bag", "fill"],
-        ["jelly bean", "pile", "make"],
-        ["book", "shelf", "fill"],
-        ["can of food", "box", "fill"]
+        ["椅子", "排", "做"],
+        ["禮物", "袋子", "裝滿"],
+        ["軟糖", "堆", "做"],
+        ["書", "書架", "裝滿"],
+        ["餅乾", "盒", "裝滿"]
     ]);
 
     var stores = new IncrementalShuffler([
         {
-            name: "office supply",
-            items: new IncrementalShuffler(["pen", "pencil", "notebook"])
+            name: "文具",
+            items: new IncrementalShuffler(["原子筆", "鉛筆", "筆記本"])
         },
         {
-            name: "hardware",
-            items: new IncrementalShuffler(["hammer", "nail", "saw"])
+            name: "五金",
+            items: new IncrementalShuffler(["鐵鎚", "釘子", "鋸子"])
         },
         {
-            name: "grocery",
-            items: new IncrementalShuffler(["banana", "loaf of bread", "gallon of milk", "potato"])
+            name: "食品",
+            items: new IncrementalShuffler(["香蕉", "麵包", "牛奶", "馬鈴薯"])
         },
         {
-            name: "gift",
-            items: new IncrementalShuffler(["toy", "game", "souvenir"])
+            name: "禮品",
+            items: new IncrementalShuffler(["玩具", "遊戲", "紀念品"])
         },
         {
-            name: "toy",
-            items: new IncrementalShuffler(["stuffed animal", "video game", "race car", "doll"])
+            name: "玩具",
+            items: new IncrementalShuffler(["泰迪熊", "電動玩具", "賽車", "洋娃娃"])
         }
     ]);
 
     var pizzas = new IncrementalShuffler([
-        "pizza",
-        "pie",
-        "cake"
+        "比薩",
+        "派",
+        "蛋糕"
     ]);
 
     var timesofday = new IncrementalShuffler([
-        "in the morning",
-        "around noon",
-        "in the evening",
-        "at night"
+        "上午",
+        "中午",
+        "傍晚",
+        "晚上"
     ]);
 
     var exercises = new IncrementalShuffler([
-        "push-up",
-        "sit-up",
-        "squat",
-        "jumping jack"
+        "伏地挺身",
+        "仰臥起坐",
+        "交互蹲跳",
     ]);
 
     var fruits = new IncrementalShuffler([
-        "apple",
-        "banana",
-        "coconut",
-        "eggplant",
-        "kiwi",
-        "lemon",
-        "mango",
-        "nectarine",
-        "orange",
-        "pomegranate",
-        "watermelon"
+        "蘋果",
+        "梨子",
+        "椰子",
+        "茄子",
+        "奇異果",
+        "檸檬",
+        "芒果",
+        "桃子",
+        "橘子",
+        "芭樂",
+        "西瓜"
     ]);
 
     var deskItems = new IncrementalShuffler([
-        "binder",
-        "crayon",
-        "eraser",
-        "folder",
-        "glue stick",
-        "marker",
-        "notebook",
-        "pencil",
-        "rubber stamp"
+        "夾子",
+        "蠟筆",
+        "橡皮擦",
+        "資料夾",
+        "澆水",
+        "簽字筆",
+        "筆記本",
+        "鉛筆",
+        "印章"
     ]);
 
     var colors = new IncrementalShuffler([
-        "red",
-        "orange",
-        "yellow",
-        "green",
-        "blue",
-        "purple",
-        "white",
-        "black",
-        "brown",
-        "silver",
-        "gold",
-        "pink"
+        "紅色",
+        "橘色",
+        "黃色",
+        "綠色",
+        "藍色",
+        "紫色",
+        "白色",
+        "黑色",
+        "褐色",
+        "銀色",
+        "金色",
+        "粉紅色"
     ]);
 
     var schools = new IncrementalShuffler([
-        "Loyola",
-        "Gardner Bullis",
-        "Almond",
-        "Covington",
-        "Springer",
-        "Santa Rita",
-        "Oak"
+        "秀山國小",
+        "秀朗國小",
+        "中和國小",
+        "均一國小",
+        "永和國小",
+        "興南國小",
+        "板橋國小"
     ]);
 
     var furnitureStore = new IncrementalShuffler([
-        "chair",
-        "table",
-        "bed frame",
-        "sofa",
-        "couch",
-        "desk",
-        "book shelf"
+        "椅子",
+        "茶几",
+        "床",
+        "沙發",
+        "躺椅",
+        "書桌",
+        "書架"
     ]);
 
     var electronicStore = new IncrementalShuffler([
-        "television",
-        "computer",
-        "laptop",
-        "camera"
+        "液晶電視",
+        "桌上型電腦",
+        "筆記型電腦",
+        "數位相機"
     ]);
 
     var clothes = new IncrementalShuffler([
-        "hat",
-        "pair of pants",
-        "belt",
-        "necklace",
-        "purse",
-        "pair of shoes",
-        "blouse",
-        "skirt",
-        "watch",
-        "pair of socks",
-        "sweatshirt",
-        "sweater",
-        "tie",
-        "scarf",
-        "dress"
+        "帽子",
+        "褲子",
+        "皮帶",
+        "項鍊",
+        "皮包",
+        "鞋子",
+        "襯衫",
+        "裙子",
+        "手錶",
+        "襪子",
+        "毛衣",
+        "領帶",
+        "洋裝"
     ]);
 
     var sides = new IncrementalShuffler([
-        "left",
-        "right"
+        "左",
+        "右"
     ]);
 
     var shirtStyles = new IncrementalShuffler([
-        "long-sleeved",
-        "short-sleeved"
+        "長袖",
+        "短袖"
     ]);
 
     // animal, avg-lifespan, stddev-lifespan
     // (data is from cursory google searches and wild guessing)
     var animals = new IncrementalShuffler([
-        ["alligator", 68, 20],
-        ["anteater", 15, 10],
-        ["bear", 40, 20],
-        ["elephant", 60, 10],
-        ["gorilla", 20, 5],
-        ["lion", 12, 5],
-        ["lizard", 3, 1],
-        ["meerkat", 13, 5],
-        ["porcupine", 20, 5],
-        ["seal", 15, 10],
-        ["sloth", 16, 5],
-        ["snake", 25, 10],
-        ["tiger", 22, 5],
-        ["turtle", 100, 20],
-        ["zebra", 25, 10]
+        ["鱷魚", 68, 20],
+        ["食蟻獸", 15, 10],
+        ["熊", 40, 20],
+        ["大象", 60, 10],
+        ["大猩猩", 20, 5],
+        ["獅子", 12, 5],
+        ["蜥蜴", 3, 1],
+        ["羚羊", 13, 5],
+        ["豪豬", 20, 5],
+        ["海豹", 15, 10],
+        ["猴子", 16, 5],
+        ["蛇", 25, 10],
+        ["老虎", 22, 5],
+        ["烏龜", 100, 20],
+        ["斑馬", 25, 10]
     ]);
 
     var farmers = new IncrementalShuffler([
-        {farmer: "farmer", crops: new IncrementalShuffler(["tomato", "potato", "carrot", "bean", "corn stalk"]), field: "field"},
-        {farmer: "gardener", crops: new IncrementalShuffler(["rose", "tulip", "daisy", "iris", "lily"]), field: "garden"}
+        {farmer: "農夫", crops: new IncrementalShuffler(["番茄", "馬鈴薯", "胡蘿蔔", "豆子", "稻子"]), field: "農地"},
+        {farmer: "園丁", crops: new IncrementalShuffler(["玫瑰花", "鬱金香", "菊花", "向日葵", "百合花"]), field: "花園"}
     ]);
 
     var distances = new IncrementalShuffler([
-        "mile",
-        "kilometer"
+        "英里",
+        "公里"
     ]);
 
     var distanceActivities = new IncrementalShuffler([
-        {present: "ride", past: "rode", noun: "bike", done: "biked", continuous: "biking"},
-        {present: "row", past: "rowed", noun: "boat", done: "rowed", continuous: "rowing"},
-        {present: "drive", past: "drove", noun: "car", done: "driven", continuous: "driving"},
-        {present: "walk", past: "walked", noun: "dog", done: "walked", continuous: "walking"}
+        {present: "騎", past: "騎", noun: "腳踏車", done: "騎", continuous: "騎"},
+        {present: "划", past: "划", noun: "船", done: "划", continuous: "划"},
+        {present: "開", past: "開", noun: "汽車", done: "開", continuous: "開"},
+        {present: "遛", past: "遛", noun: "狗", done: "遛", continuous: "遛"}
     ]);
 
     var indefiniteArticle = function(word) {
@@ -471,23 +454,23 @@ $.fn["word-problemsLoad"] = function() {
         },
 
         he: function(i) {
-            return people.get(i - 1).get(0)[1] === "m" ? "he" : "she";
+            return people.get(i - 1).get(0)[1] === "m" ? "他" : "她";
         },
 
         He: function(i) {
-            return people.get(i - 1).get(0)[1] === "m" ? "He" : "She";
+            return people.get(i - 1).get(0)[1] === "m" ? "他" : "她";
         },
 
         him: function(i) {
-            return people.get(i - 1).get(0)[1] === "m" ? "him" : "her";
+            return people.get(i - 1).get(0)[1] === "m" ? "他" : "她";
         },
 
         his: function(i) {
-            return people.get(i - 1).get(0)[1] === "m" ? "his" : "her";
+            return people.get(i - 1).get(0)[1] === "m" ? "他的" : "她的";
         },
 
         His: function(i) {
-            return people.get(i - 1).get(0)[1] === "m" ? "His" : "Her";
+            return people.get(i - 1).get(0)[1] === "m" ? "他的" : "她的";
         },
 
         An: function(word) {
